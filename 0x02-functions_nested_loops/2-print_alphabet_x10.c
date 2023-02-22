885d13edc5bed9caf39845d81307f882a321d059
diana-dee <dianahchepkoech2017@gmail.com>
2023-02-21 16:27:53 -0800

    Functions

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints the lowercase alphabets
+ *
+ *Return: Always 0 (Success)
+ */
+
+void print_alphabet_x10(void)
+{
+	int j;
+	int k;
+
+	for (k = 0; k < 10; k++)
+	{
+		for (j = 'a'; j <= 'z'; j++)
+		{
+			_putchar(j);
+		}
+		_putchar ('\n');
+	}
+}
