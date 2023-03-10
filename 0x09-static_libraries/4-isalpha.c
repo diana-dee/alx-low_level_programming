064dee9aa995a46b58c9fe0dec630100abdb58c8
diana-dee <dianahchepkoech2017@gmail.com>
2023-03-10 14:58:01 -0800

    A library is not a luxury but one of the necessities of life

---
+++ 0x09-static_libraries/4-isalpha.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks for alphabetical letters
+ * @c: a charter to be checked
+ * Return: returns 0 or 1 depending on condition
+ */
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+	{
+	return (1);
+	}
+	return (0);
+}
