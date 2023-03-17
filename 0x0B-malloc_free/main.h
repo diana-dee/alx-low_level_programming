4c6af465ed26d09025dc98307da844aa82c82d28
diana-dee <dianahchepkoech2017@gmail.com>
2023-03-17 04:40:27 -0700

    Memory allocation

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,28 +1,12 @@
-#include "main.h"
-#include <stdlib.h>
-/**
- * create_array - creates an array of chars.
- * @size: size of the array.
- * @c: storaged char
- *
- * Return: pointer of an array of chars
- */
-char *create_array(unsigned int size, char c)
-{
-	char *cr;
-	unsigned int i;
+#ifndef MAIN_H
+#define MAIN_H
 
-	if (size == 0)
-		return (NULL);
-
-	cr = malloc(sizeof(c) * size);
-
-	if (cr == NULL)
-		return (NULL);
-
-	for (i = 0; i < size; i++)
-		cr[i] = c;
-
-	return (cr);
-}
-
+int _putchar(char c);
+char *create_array(unsigned int size, char c);
+char *_strdup(char *str);
+char *str_concat(char *s1, char *s2);
+int **alloc_grid(int width, int height);
+void free_grid(int **grid, int height);
+char *argstostr(int ac, char **av);
+char **strtow(char *str);
+#endif
